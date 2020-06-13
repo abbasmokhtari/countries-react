c668b7ff8f328ed9c340f982937693973dc88ecf
Abbas Mokhtari <abs.mokhtari@gmail.com>
2020-06-13 22:04:33 +0100

    week one finished

--- src/components/CountryBlock.js
+++ src/components/CountryBlock.js
@@ -8,10 +8,12 @@ function CountryBlock() {
         return (
           <div key={e.cioc} className='countryBlock'>
             <img src={e.flag} alt='This is a flag'/>
+            <div className='countryInfo'>
             <h3>{e.name}</h3>
-            <p>Population: {e.population}</p>
-            <p>Region: {e.region}</p>
-            <p>Capital: {e.capital}</p>
+            <p>Population: <span className='dataRes'>{e.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span></p>
+            <p>Region: <span className='dataRes'>{e.region}</span></p>
+            <p>Capital: <span className='dataRes'>{e.capital}</span></p>
+            </div>
           </div>
         );
       })}
