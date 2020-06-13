46b33acdfbd4d72f4efc2308e6a28ced6801aa0e
Abbas Mokhtari <abs.mokhtari@gmail.com>
2020-06-13 14:35:28 +0100

    Header done

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className='header'>
+            <h2>Where in the World?</h2>
+        </div>
+    )
+}
+
+export default Header
